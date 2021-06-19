0b6ae6bd02e2b2a1fedcfdc2f0aff4ab42acce5b
Nilomiranda <nilomiranda3@gmail.com>
2021-06-18 22:08:14 -0300

    returning error message on wrong credentials

--- app/graphql/mutations/sign_in.rb
+++ app/graphql/mutations/sign_in.rb
@@ -6,6 +6,7 @@ module Mutations
 
     field :token, String, null: true
     field :user, Types::UserType, null: true
+    field :error, String, null: true
 
     def resolve(credentials: nil)
       return unless credentials
@@ -13,14 +14,20 @@ module Mutations
       user = User.find_by email: credentials[:email]
 
       return unless user
-      return unless user.authenticate(credentials[:password])
+      return {
+        error: 'Wrong credentials'
+      } if check_password(user, credentials[:password]).nil?
 
-      token_payload = { user_id: user[:id] }
+      token_payload = { user_id: user[:id], exp: Time.now.to_i + 1.week }
       token = JWT.encode token_payload, nil, 'none'
 
-      puts("token", token)
-
       { user: user, token: token }
+    end
+
+    def check_password(user, password)
+      return nil unless user.authenticate(password)
+
+      true
     end
   end
 end
