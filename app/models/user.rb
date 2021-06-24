0b17b038d99046fcc94a0f05009809dee3ce8bdd
Nilomiranda <nilomiranda3@gmail.com>
2021-06-23 21:23:49 -0300

    email validation to user modeL

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@ class User < ApplicationRecord
   has_secure_password
 
   validates :name, presence: true
-  validates :email, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true, email: true
 end
