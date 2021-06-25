9a5b64b01ed3fce194261a23a2ddda92e45bf253
Nilomiranda <nilomiranda3@gmail.com>
2021-06-25 18:10:20 -0300

    remove links query

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -5,10 +5,6 @@ module Types
     add_field GraphQL::Types::Relay::NodeField
     add_field GraphQL::Types::Relay::NodesField
 
-    # queries are just represented as fields
-    # `all_links` is automatically camelcased to `allLinks`
-    field :all_links, resolver: Resolvers::AllLinks
-
     field :posts, Types::PostType.connection_type, null: false, max_page_size: 20
     def posts
       ProtectedResource.ensure_authenticated context
