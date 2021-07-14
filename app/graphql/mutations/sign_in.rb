b48536a1d4908acbe68cca1eedd00671de8f371e
Nilomiranda <nilomiranda3@gmail.com>
2021-07-14 20:51:38 -0300

    changed unauthorized message

--- app/graphql/mutations/sign_in.rb
+++ app/graphql/mutations/sign_in.rb
@@ -14,7 +14,7 @@ module Mutations
 
       return unless user
 
-      raise GraphQL::ExecutionError.new('Unauthorized', options: { status: :unauthorized, code: 401 }) if check_password(user, credentials[:password]).nil?
+      raise GraphQL::ExecutionError.new('Either email or password is wrong', options: { status: :unauthorized, code: 401 }) if check_password(user, credentials[:password]).nil?
 
       token_payload = { user_id: user[:id], exp: Time.now.to_i + 1.week }
       token = JWT.encode token_payload, nil, 'none'
